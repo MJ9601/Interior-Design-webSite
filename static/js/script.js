8a2ff734b1d6dc485a0b906ab8b351ac890764e8
mohammadjavad <m_javad_96@yahoo.com>
2021-07-29 21:00:30 +0430

    building Interior Design website (first_of_contact_section)

--- static/js/script.js
+++ static/js/script.js
@@ -5,7 +5,7 @@ document.querySelector('#Xmark').addEventListener('click', hiddenSideBarFunc);
 function dropDownMenuFunc() {
     document.querySelector('.container').className +=' sideBarShowed';
     console.log('Click');
-    document.querySelector('#dropDownIcon').classList.toggle('navbarHidden');
+    document.querySelector('#dropDownIcon').classList.toggle('navbarHidden'); /* If included, turns the toggle into a one way-only operation. If set to false, then token will only be removed, but not added. If set to true, then token will only be added, but not removed. */
     console.log(document.querySelector('.container').className);
     
 }
